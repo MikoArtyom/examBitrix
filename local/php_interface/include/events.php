ec060e24ad1b7c0956334aa3ba9b9ef63468cc58
Artem99 <pasha.pikav@mail.ru>
2021-04-14 16:39:38 +0300

    [ex2-51] Изменение данных в письме

--- local/php_interface/include/events.php
+++ local/php_interface/include/events.php
@@ -1,6 +1,7 @@
 <?php
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("EX2", "EX2_50"));
 AddEventHandler("main", "OnEpilog", Array("EX2", "EX2_93"));
+AddEventHandler("main", "OnBeforeEventAdd", array("EX2", "EX2_51"));
 
 IncludeModuleLangFile(__FILE__);
 
@@ -25,7 +26,7 @@ class EX2
 
 				if ($arItems["SHOW_COUNTER"] > MAX_COUNT){
 					global $APPLICATION;
-					$APPLICATION->ThrowException(GetMessage("ERROR_MSG", ["COUNT" => $arItems["SHOW_COUNTER"]]));
+					$APPLICATION->ThrowException(GetMessage("ERROR_MSG"). $arItems["SHOW_COUNTER"]);
 					return false;
 
 				}
@@ -37,10 +38,6 @@ class EX2
 	{
 		if (defined("ERROR_404") && ERROR_404 == "Y"){
 			global $APPLICATION;
-			$APPLICATION->RestartBuffer();
-			include $_SERVER["DOCUMENT_ROOT"] . SITE_TEMPLATE_PATH . "/header.php";
-			include $_SERVER["DOCUMENT_ROOT"] . "/404.php";
-			include $_SERVER["DOCUMENT_ROOT"] . SITE_TEMPLATE_PATH . "/footer.php";
 			CEventLog::Add(
 				array(
 					"SEVERITY" => "INFO",
@@ -49,7 +46,84 @@ class EX2
 					"DESCRIPTION" => $APPLICATION->GetCurDir()
 				)
 			);
+			$APPLICATION->RestartBuffer();
+			include $_SERVER["DOCUMENT_ROOT"] . SITE_TEMPLATE_PATH . "/header.php";
+			include $_SERVER["DOCUMENT_ROOT"] . "/404.php";
+			include $_SERVER["DOCUMENT_ROOT"] . SITE_TEMPLATE_PATH . "/footer.php";
+
 		}
 	}
+
+//	function EX2_51(&$event, &$arFields){
+//		if ($event == "FEEDBACK_FROM"){
+//			global $USER;
+//			if ($USER->IsAuthorized()){
+//				$arFields["AUTHOR"] = GetMessage("AUTH",
+//					array(
+//						"#ID#" => $USER->GetID(),
+//						"#LOGIN" => $USER->GetLogin(),
+//						"#NAME#" => $USER->GetFirstName(),
+//						"FROM_NAME" => $arFields["AUTHOR"]
+//					)
+//				);
+//			}
+//			else{
+//				$arFields["AUTHOR"] = GetMessage("NOT_AUTH",
+//					array(
+//						"FROM_NAME" => $arFields["AUTHOR"]
+//					)
+//				);
+//			}
+//
+//			CEventLog::Add(
+//				array(
+//					"SEVERITY" => "SECURITY",
+//					"AUDIT_TYPE_ID" => "REPLACEMENT",
+//					"MODULE_ID" => "main",
+//					"ITEM_ID" => $event,
+//					"DESCRIPTION" => GetMessage("REPLACEMENT", ["AUTHOR" => $arFields["AUTHOR"]]),
+//				)
+//			);
+//
+//		}
+//	}
+
+	function EX2_51(&$event, &$lid, &$arFields)
+	{
+		if ($event == "FEEDBACK_FORM")
+		{
+			global $USER;
+			if ($USER->isAuthorized())
+			{
+				$arFields["AUTHOR"] = GetMessage("AUTH",
+					[
+						"#ID#" => $USER->GetID(),
+						"#LOGIN#" => $USER->GetLogin(),
+						"#NAME#" => $USER->GetFullName(),
+						"#FORM_NAME#" => $arFields["AUTHOR"]
+					]
+				);
+			}
+			else
+			{
+				$arFields["AUTHOR"] = GetMessage("NOT_AUTH",
+					[
+						"#FORM_NAME#" => $arFields["AUTHOR"]
+					]
+				);
+			}
+			CEventLog::Add(
+				array(
+					"SEVERITY" => "INFO",
+					"AUDIT_TYPE_ID" => "REPLACEMENT",
+					"MODULE_ID" => "main",
+					"ITEM_ID" => $event,
+					"DESCRIPTION" => GetMessage("REPLACEMENT", ["AUTHOR" => $arFields["AUTHOR"]])
+				)
+			);
+		}
+
+	}
+
 
 }
