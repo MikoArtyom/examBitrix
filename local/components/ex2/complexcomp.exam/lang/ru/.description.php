d26db64be5925e05b6a1016daf5a67c6e8cdf69b
Artem99 <pasha.pikav@mail.ru>
2021-04-21 17:39:14 +0300

    [ex2-25] Создание комплексного компонента «Моя фотогалерея»

---
+++ local/components/ex2/complexcomp.exam/lang/ru/.description.php
@@ -0,0 +1,6 @@
+<?
+$MESS ['IBLOCK_PHOTO_NAME'] = "Фотогалерея";
+$MESS ['IBLOCK_PHOTO_DESCRIPTION'] = "Полная фотогалерея";
+$MESS ['T_IBLOCK_DESC_PHOTO'] = "Фотогалерея";
+$MESS ['IBLOCK_PHOTO_SECTION'] = "Комплексный компонент";
+?>
---
+++ local/components/ex2/complexcomp.exam/templates/.default/exampage.php
@@ -0,0 +1,6 @@
+<p>
+	PARAM1 = <?= $arResult["VARIABLES"]["PARAM1"]?>
+</p>
+<p>
+	PARAM2 = <?= $arResult["VARIABLES"]["PARAM2git"]?>
+</p>
